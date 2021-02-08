9e79d6d9b15f7240d2b78db00ce3694fafe27f8b
sleonovets <cappuccilala@gmail.com>
2021-02-08 12:58:40 +0300

    Collections/DataStructures homework 4

--- src/main/scala/Collections.scala
+++ src/main/scala/Collections.scala
@@ -28,7 +28,6 @@ object Collections {
   }
 
   // https://leetcode.com/problems/widest-vertical-area-between-two-points-containing-no-points
-  // can we use fold instead of map?
   def maxWidthOfVerticalArea(points: Array[Array[Int]]): Int = {
     val sortedX = points.map(arr => arr(0)).sorted
     sortedX.zipWithIndex.map {
@@ -91,6 +90,29 @@ object Collections {
         mat(0).indices.toArray map {
           j => calculate(i, j)
         }
+    }
+  }
+
+  // Homework
+  //
+  // Implement a special sort which sorts the keys of a map (K) according to their associated
+  // values (V).
+  //
+  // In case of "ties" (equal values) it should group these keys K into Set-s in the results.
+  //
+  // The input is a map where keys (K) are the values to be sorted and values are their associated numeric
+  // values.
+  //
+  // The output is a list (in ascending order according to the associated `Int` values) of tuples of `Set`-s
+  // with values from K, and the associated value V for these values in the `Set`.
+  //
+  // For example:
+  //
+  // Input `Map("a" -> 1, "b" -> 2, "c" -> 4, "d" -> 1, "e" -> 0, "f" -> 2, "g" -> 2)` should result in
+  // output `List(Set("e") -> 0, Set("a", "d") -> 1, Set("b", "f", "g") -> 2, Set("c") -> 4)`.
+  def sortConsideringEqualValues[T](map: Map[T, Int]): List[(Set[T], Int)] = {
+    map.groupBy(_._2).toList.map {
+      case (i, grouped) => (grouped.keys.toSet -> i)
     }
   }
 }
